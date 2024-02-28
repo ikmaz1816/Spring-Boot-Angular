87c66db018f2e0f5d9f4438a2464783493991766
ikmaz1816 <ikmaz1816@gmail.com>
2024-02-28 22:42:21 +0530

    Added CSRF token with http false

--- Backend/src/main/java/com/auth/registration/configuration/SecurityConfiguration.java
+++ Backend/src/main/java/com/auth/registration/configuration/SecurityConfiguration.java
@@ -4,25 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(csrf -> csrf.disable())
+        //with the below line the browser will be able to access the csrf token
+        return httpSecurity.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry.requestMatchers("/user/**")
                                 .permitAll()
                         )
                 .cors(Customizer.withDefaults())
+                //.headers(head->head.frameOptions(frameOptionsConfig -> frameOptionsConfig.sameOrigin()))
                 .build();
     }
 
@@ -38,4 +42,6 @@ public class SecurityConfiguration {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",configuration);
         return urlBasedCorsConfigurationSource;
     }
+
+
 }
