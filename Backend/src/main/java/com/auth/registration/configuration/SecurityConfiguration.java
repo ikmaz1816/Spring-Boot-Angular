3fd373a18712749ba451b3674757cd462bb45bca
ikmaz1816 <ikmaz1816@gmail.com>
2024-02-28 22:05:46 +0530

    Made changes to Cors using Cors Configuration

--- Backend/src/main/java/com/auth/registration/configuration/SecurityConfiguration.java
+++ Backend/src/main/java/com/auth/registration/configuration/SecurityConfiguration.java
@@ -2,8 +2,15 @@ package com.auth.registration.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
@@ -15,6 +22,20 @@ public class SecurityConfiguration {
                         authorizationManagerRequestMatcherRegistry.requestMatchers("/user/**")
                                 .permitAll()
                         )
+                .cors(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        configuration.setAllowedMethods(List.of("GET","POST"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource
+                =new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",configuration);
+        return urlBasedCorsConfigurationSource;
     }
 }
